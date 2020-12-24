cb200f05d6b1fec25beb4ac2604ff4ce8c7b12d8
denalien <denalien100@gmail.com>
2020-12-24 19:33:00 +0300

    homework 3.5 finished

---
+++ src/main/java/partthree/lesson5/Car.java
@@ -0,0 +1,47 @@
+package partthree.lesson5;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
+import static partthree.lesson5.MainClass.*;
+
+public class Car implements Runnable {
+    private static int CARS_COUNT;
+    static {
+        CARS_COUNT = 0;
+    }
+    private Race race;
+    private int speed;
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public Car(Race race, int speed) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+    }
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int)(Math.random() * 800));
+            System.out.println(this.name + " готов");
+            prepare.countDown();
+            prepare.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+        }
+        if(finish.tryLock()){
+            System.out.println(this.name + "WIN");
+        }
+        end.countDown();
+    }
+}
---
+++ src/main/java/partthree/lesson5/Tunnel.java
@@ -0,0 +1,28 @@
+package partthree.lesson5;
+
+import static partthree.lesson5.MainClass.smp;
+
+public class Tunnel extends Stage {
+    public Tunnel() {
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+    @Override
+    public void go(Car c) {
+        try {
+            try {
+                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                smp.acquire();
+                System.out.println(c.getName() + " начал этап: " + description);
+                Thread.sleep(length / c.getSpeed() * 1000);
+                smp.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(c.getName() + " закончил этап: " + description);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
